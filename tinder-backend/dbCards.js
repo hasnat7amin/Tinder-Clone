3c512c782078d75f4bae6d64dacdb4e453c67f1e
Hasnat Amin <hasnatamin065@gmail.com>
2021-03-07 12:23:23 +0500

    deleting files and adding images

--- tinder-backend/dbCards.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-const schema = mongoose.Schema();
-const cardSchema = new schema({
-    name : String,
-    Url : String
-})
-
-export default mongoose.model("cardSchema", cardSchema)
